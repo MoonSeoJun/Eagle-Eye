c8c1c0c8a233e0c80e5245fc15e24cd7b42aa2f3
MoonSeoJun <msj00130@gmail.com>
2022-03-17 19:24:29 +0900

    [COMPLETE] version 1

--- eagle_eye/main.py
+++ eagle_eye/main.py
@@ -12,7 +12,10 @@ if __name__ == "__main__":
 
     pint_finder = PointFinder(video_path, pitch_img)
 
+    print(video_path)
+
     input_point = pint_finder.find_point("video")
+    print("---")
     pitch_point = pint_finder.find_point("image")
 
     homography_class = Homography(input_point, pitch_point)
